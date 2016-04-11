809d1d1c6b51298c35f349a118337889cc641ff4
mikeaussie <michael.pyatin@me.com>
2016-04-11 11:21:44 +1000

    Initial Commit

---
+++ devBootcamps/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  devBootcamps
+//
+//  Created by Mike Piatin on 11/04/2016.
+//  Copyright © 2016 Aurora Software. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
